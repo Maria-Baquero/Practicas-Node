791cafbd25d03b56b3fe397538d4a8d7cbf2df45
Maria Baquero <maria.baquerocalvo@gmail.com>
2026-01-09 15:52:30 +0100

    carpeta actualizada

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,7 +1,7 @@
 require('colors');
 
 //con inquirer esta linea ya no es necesaria:
-// const { mostrarMenu, pausa } = require('./helpers/mensajes');
+//const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const { inquirerMenu } = require('./helpers/inquirer');
 
@@ -11,19 +11,35 @@ console.clear();
 
 
 const main = async() => {
-    console.log('Hola Mundo'.green);
+
 
     let opt = '';
 
     //esto es para hacer el menu infinito,
     // cada vez que se termine de hacer una accion volvera a mostrarse el menu
+
+
+    //esta forma se hace con el archivo mensajes.js
+    /*
     do{
-        opt = await inquirerMenu();
+        opt = await mostrarMenu();
         console.log({ opt });
         
         if( opt !== '0') await pausa();
 
     }while (opt !== '0');
+    */
+
+
+
+    //esta forma se hace con el archivo inquirer.js
+    do{
+        opt = await inquirerMenu();
+        console.log({ opt });
+        
+    }while (opt !== '0');
+
+
 
 };
 
--- 04-tareas-hacer/helpers/inquirer.js
+++ 04-tareas-hacer/helpers/inquirer.js
@@ -1,28 +1,26 @@
 require('colors');
-const inquirer  = require('inquirer');
 
-
-const preguntas = [
-        {
-            type: 'list',
-            name: 'opcion',
-            message: '¿Qué desea hacer?',
-            choices: ['opt1', 'opt2', 'opt3']
-        }
-    ];
 
 const inquirerMenu = async () => {
 
     console.clear();
 
+    const { select } = await import('@inquirer/prompts');
+
     console.log('========================'.green);
     console.log('  Seleccione una opción  '.white);
     console.log('========================\n'.green);
 
+    const opcion = await select({
+        message: '¿Qué desea hacer?',
+        choices: [
+            { name: 'Opción 1', value: 'opt1' },
+            { name: 'Opción 2', value: 'opt2' },
+            { name: 'Opción 3', value: 'opt3' }
+        ]
+    });
 
-
-    const opt = await inquirer.prompt(preguntas);
-    return opt;
+    return opcion;
 
 };
 
