33d62e4370553a3f81262722d226fb450e882e75
Maria Baquero <maria.baquerocalvo@gmail.com>
2026-01-21 14:26:19 +0100

    carpeta nueva y actualizacion de archivos

--- 07-restserver-old-v2/routes/auth.js
+++ 07-restserver-old-v2/routes/auth.js
@@ -1,12 +1,9 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
-const { login, googleSignIn } = require('../controllers/auth');
+const { login } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validate-fields')
 
 const router = Router();
-
-
-
 
 
 router.post('/login', [
@@ -14,21 +11,6 @@ router.post('/login', [
     check('password', 'Password is required').not().isEmpty(),
     validateFields
 ],login );
-
-
-
-router.post('/google', [
-    check('id_token', 'id_token is required').not().isEmpty(),not().isEmpty(),
-    validateFields
-], googleSignIn);
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
