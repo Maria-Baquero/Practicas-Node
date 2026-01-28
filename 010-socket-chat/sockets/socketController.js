16f26c67beb7542b622a80f2c28520cdd7edf8f6
Maria Baquero <maria.baquerocalvo@gmail.com>
2026-01-28 14:37:49 +0100

    WIP - signin

---
+++ 010-socket-chat/sockets/socketController.js
@@ -0,0 +1,14 @@
+const { Socket } = require("socket.io")
+
+const socketController = (socket = new Socket) => {
+
+    //console.log('client connected', socket.id);
+
+
+    
+
+}
+
+module.exports = {
+    socketController
+}
